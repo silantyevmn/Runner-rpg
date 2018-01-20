8e6215654c9e7a73327741928c20996eb483a337
silantyevmn <5992033@mail.ru>
2018-01-21 00:40:28 +0300

    android

--- android/src/ru/silantyevmn/game/AndroidLauncher.java
+++ android/src/ru/silantyevmn/game/AndroidLauncher.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import ru.silantyevmn.game.RunnerGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
